644ee09c674a2bf1909e6902523bac13de748669
Luiz Oliveira <valuiz1@gmail.com>
2021-12-11 21:33:50 +0000

    Heroku push inicial

--- app/models.py
+++ app/models.py
@@ -26,6 +26,10 @@ class Cliente(models.Model):
     # Caixa de valor binário (verdadeiro ou falso)
     habilitado = models.BooleanField('habitação',blank=True,null=True, choices=( (True, 'sim'), (False, 'não') ))
     obs = models.TextField('observação',blank=True,null=True) # Caixa de texto grande (parágrafo)
+    # Essa é uma dificuldade que eu passei muito tempo tendo, como fazer um relacionamento de amizade entre usuarios?
+    # Claramente se trata de um relacionamento many to many, mas eu não sabia como se referencia a si mesmo em Django
+    # E aqui está resolvido, se usa self, assim como em python, mas se coloca em string.
+    amigos = models.ManyToManyField("self",blank=True,null=True)
 
     # Esse método tem como objetivo mudar a apresentação de cada objeto deste modelo.
     # Aqui, escolhemos representá-lo pelo atributo 'nome'
@@ -47,8 +51,11 @@ class Author(Cliente):
 
 class Books(models.Model):
     titulo = models.CharField('título',max_length=200)
-    # Aqui ligamos o modelo books ao modelo autor através de uma ForeignKey.
-    # Isto é, ligamos um livro ao seu autor
+    # Aqui ligamos o modelo books ao modelo autor através de uma ForeignKey. Criando uma relação many-to-one
+    # Isto é, ligamos um ou mais livros ao seu autor. Mas existem outro tipo: Many to many
+    # Many to many: relação de amigos em uma rede social.
+    # Também é possível ver o argumento "on_delete", que diz o que fazer no caso de objetos dependentes serem apagados.
+    # Abaixo temos todas as opções:
     # PROTECT: Não deleta o modelo até que todos os objetos dependentes sejam deletados.
     # CASCADE: Ao deletar o objeto, todos os seus dependentes também são deletados
     # RESTRICT: Similar ao PROTECT, mas se aproximando à ação de mesmo nome em SQL
@@ -102,7 +109,7 @@ class Albums(models.Model):
 # >>> joao = Cliente(nome = 'João', data ='1999-09-09',pontuation= 89,habilitado= True, obs='observado')
 # >>> joao.save()
 # Ou podemos fazer tudo em uma só linha:
-# pedro = Cliente.objects.create('Pedro', '18/04/85', 14, False, 'observei')
+# pedro = Cliente.objects.create(nome='Pedro',data='1985-04-18',pontuation=14,habilitado=False,obs='observei')
 
 # É possível fazer uma função de Query ao misturar funcionalidades Django com uma simples Lambda function:
 # >>> listar = lambda x: Cliente.objects.filter(id=x)
